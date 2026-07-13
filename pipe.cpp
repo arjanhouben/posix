7dbc0310f8db126418bb17254b59341175769814
Arjan Houben <git@arjanhouben.nl>
2026-07-13 19:14:42 +0200

    added pipe tests

---
+++ pipe.cpp
@@ -0,0 +1,43 @@
+#include <unistd.h>
+
+#include "catch2/catch.hpp"
+#include "arjan/posix/pipe.hpp"
+
+TEST_CASE( "pipe" )
+{
+	WHEN( "using the default constructor" )
+	{
+		arjan::posix::pipe p;
+		CHECK_FALSE( bool( p ) );
+		CHECK( p[ arjan::posix::pipe::input ].get() == arjan::posix::file::invalid );
+		CHECK( p[ arjan::posix::pipe::output ].get() == arjan::posix::file::invalid );
+	}
+
+	WHEN( "opening a pipe" )
+	{
+		arjan::posix::pipe p;
+		p.open();
+		CHECK( bool( p ) );
+		CHECK( p[ arjan::posix::pipe::input ].get() != arjan::posix::file::invalid );
+		CHECK( p[ arjan::posix::pipe::output ].get() != arjan::posix::file::invalid );
+		CHECK( p[ arjan::posix::pipe::input ].get() != p[ arjan::posix::pipe::output ].get() );
+
+		WHEN( "closing an opened pipe" )
+		{
+			p.close();
+			CHECK_FALSE( bool( p ) );
+			CHECK( p[ arjan::posix::pipe::input ].get() == arjan::posix::file::invalid );
+			CHECK( p[ arjan::posix::pipe::output ].get() == arjan::posix::file::invalid );
+		}
+
+		WHEN( "writing to the pipe and reading from it" )
+		{
+			const std::string test_data = "hello from pipe";
+			::write( p[ arjan::posix::pipe::output ].get(), test_data.data(), test_data.size() );
+			std::string buf( 256, 0 );
+			const ssize_t n = ::read( p[ arjan::posix::pipe::input ].get(), buf.data(), buf.size() );
+			CHECK( n > 0 );
+			CHECK( std::string( buf.data(), n ) == test_data );
+		}
+	}
+}
